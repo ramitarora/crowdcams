d836a05f2975cadd2604dab5402132e8628097a9
ramitarora <ramit91@gmail.com>
2014-03-20 21:39:00 -0500

    Cleaned the UI, made appropriate pages, added links to url mapper and views.py, modified and cleaned base template according to our needs

--- web_app/urls.py
+++ web_app/urls.py
@@ -8,5 +8,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'CommunityCams.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', views.home, name='home'),
+	url(r'^$', views.home, name='home'),
+    url(r'^home/$', views.home, name='home'),
+	url(r'^protectyourneighborhood/$', views.protect_your_neighborhood, name='protect_your_neighborhood'),
+	url(r'^howitworks/$', views.how_it_works, name='how_it_works'),
+	url(r'^projectfounders/$', views.project_founders, name='project_founders'),
+	url(r'^ourvision/$', views.our_vision, name='our_vision'),
 )
--- web_app/views.py
+++ web_app/views.py
@@ -4,7 +4,21 @@ from django.shortcuts import render
 
 
 def home(request):
-    context = {
-        'title': 'Community Cams'
-    }
+    context = {'title': 'Community Cams'}
     return render(request, 'index.html', context)
+
+def protect_your_neighborhood(request):
+	context = {'title': 'Protect your Neighborhood'}
+	return render(request, 'protectyourneighborhood.html')
+
+def how_it_works(request):
+	context = {'title': 'How It Works'}
+	return render(request, 'howitworks.html')
+	
+def project_founders(request):
+	context = {'title': 'Project Founders'}
+	return render(request, 'projectfounders.html')
+
+def our_vision(request):
+	context = {'title': 'Our Vision'}
+	return render(request, 'ourvision.html')
