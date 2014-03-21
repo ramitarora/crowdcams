c8c3126466dbfe22460d8790b712a05b2aa413e4
ramitarora <ramit91@gmail.com>
2014-03-21 03:27:19 -0500

    Added REST API calls for UStream

--- web_app/models.py
+++ web_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+#Model to integrate UStream Videos
+class UStream(models.Model):
+	ustream_id = models.IntegerField(default=0)
+	title = models.CharField(max_length=200)
+	is_protected = models.NullBooleanField(default=False)
+	url = models.CharField(max_length=200)
+	description = models.CharField(max_length=600)
+	status = models.CharField(max_length = 10)
+	image_url_small = models.CharField(max_length=200)
+	image_url_big = models.CharField(max_length=200)
