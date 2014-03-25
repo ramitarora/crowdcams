469d2cc4e9fa7c33f3d335548003170e7b7ca9b2
Anant Singh <anant_xinix@yahoo.co.in>
2014-03-25 19:18:20 -0400

    UI Changes

--- web_app/static/js/custom.js
+++ web_app/static/js/custom.js
@@ -1,13 +1,3 @@
 $(document).ready(function() {
-	$(".watch_stream").fancybox({
-		maxWidth	: 800,
-		maxHeight	: 600,
-		fitToView	: false,
-		width		: '70%',
-		height		: '70%',
-		autoSize	: false,
-		closeClick	: false,
-		openEffect	: 'none',
-		closeEffect	: 'none'
-	});
+
 });
